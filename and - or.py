b593c0debff9ef59c51ad4a7e4051d2f1485564b
m4xiz34 <maxisacroissant@gmail.com>
2025-11-28 19:28:57 +1100

    Create and - or.py

---
+++ and - or.py
@@ -0,0 +1,11 @@
+a = 24
+b = -1
+c = 0
+if a > c or b > c:
+    print("Either of the numbers are greater that 0.")
+else:
+    print("None of the numbers are greater than 0.")
+if a > 0 and b < c:
+    print ("This is correct.")
+else:
+    print("This is incorrect.")
